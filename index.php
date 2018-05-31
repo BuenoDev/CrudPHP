06113478b8aab58ef04ae35142400618f161293f
Gustavo Bueno <gbueno360@gmail.com>
2018-05-31 11:09:32 -0300

    git init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+</head>
+<body>
+    <form action="" class='form'>
+        <input type="text" min=3 placeholder = 'nome' class='name'><br>
+        <input type="email" min=4 placeholder = 'email' class = 'email'><br>
+        <input type="submit" class='submit' value ='send'><br>
+    </form>
+    <script src="/public/js/script.js"></script>
+</body>
+</html>
---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.submit').submit(function(event){
+        event.preventDefault();
+        var name = $('.name');
+        var email = $('.email');
+        $.post('')
+    });
+});
